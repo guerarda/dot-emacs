2d2650de10837c47900daef83ac64a97e649ac2f
guerarda <guerarda@users.noreply.github.com>
2015-12-02 08:30:41 -0800

    Org-mode customization
    - Todo states and faces

--- custom.el
+++ custom.el
@@ -3,7 +3,9 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(ledger-binary-path "/usr/local/bin/ledger"))
+ '(ledger-binary-path "/usr/local/bin/ledger")
+ '(org-todo-keyword-faces (quote (("INPROGRESS" . "#6c71c4"))))
+ '(org-todo-keywords (quote ((sequence "TODO" "INPROGRESS" "DONE")))))
 
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
