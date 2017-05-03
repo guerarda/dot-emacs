b9e6a97c8ad426ee0c1205ed2e2f37e57582fcb4
guerarda <guerarda@users.noreply.github.com>
2017-05-03 11:38:07 -0700

    C-a moves to first non-whitespace character

--- init.el
+++ init.el
@@ -45,6 +45,19 @@
 (bind-key "C-c ;" #'comment-or-uncomment-region)
 (define-key key-translation-map (kbd "ESC") (kbd "C-g"))
 
+;; C-a moves to first non-whitespace characted, then the real
+;; beginning of line
+(defadvice move-beginning-of-line (around smarter-bol activate)
+  ;; Move to requested line if needed
+  (let ((arg (or (ad-get-arg 0) 1)))
+    (when (/= arg 1)
+      (forward-line (1- arg))))
+  ;; Move to indentation on first call, then to actual BOL on second.
+  (let ((pos (point)))
+    (back-to-indentation)
+    (when (= pos (point))
+      ad-do-it)))
+
 (defun my-prog-mode-hook ()
   (nlinum-mode)
   (setq-default nlinum-format "%4d\u2502")
